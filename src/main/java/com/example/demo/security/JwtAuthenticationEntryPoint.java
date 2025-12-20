d89d08722d4ffd8b4192696ef34167d81b5fb0a7
bala-amypo <bala@amypo.in>
2025-12-20 11:55:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,15 @@
+package com.example.carbonfootprint.security;
+
+import jakarta.servlet.http.*;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, Exception ex)
+            throws IOException {
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.example.carbonfootprint.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String secret = "carbon_secret";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
