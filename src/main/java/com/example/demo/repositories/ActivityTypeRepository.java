f15808aa5c7e2aeec3665e1c7d5bd6084c8d0160
bala-amypo <bala@amypo.in>
2025-12-21 14:11:06 +0000

    User commit

--- src/main/java/com/example/demo/repositories/ActivityTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.carbonfootprint.repository;
-
-import com.example.carbonfootprint.entity.ActivityType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ActivityTypeRepository extends JpaRepository<ActivityType, Long> {
-    List<ActivityType> findByCategoryId(Long categoryId);
-}
