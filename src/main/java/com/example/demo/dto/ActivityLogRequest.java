34f0938c6814ca2b7858939607ffdadd520c6b13
bala-amypo <bala@amypo.in>
2025-12-21 17:40:33 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ActivityLogRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import java.time.LocalDate;
+
+public class ActivityLogRequest {
+    private Double quantity;
+    private LocalDate activityDate;
+
+    // Getters and Setters
+    public Double getQuantity() { return quantity; }
+    public void setQuantity(Double quantity) { this.quantity = quantity; }
+    public LocalDate getActivityDate() { return activityDate; }
+    public void setActivityDate(LocalDate activityDate) { this.activityDate = activityDate; }
+}
