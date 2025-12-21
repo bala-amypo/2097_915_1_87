2cac5036abeb1236235ceb4d63f95de7bb19b5d7
bala-amypo <bala@amypo.in>
2025-12-21 06:49:00 +0000

    User commit

--- src/main/java/com/example/demo/repositories/ActivityLogRepository.java
+++ src/main/java/com/example/demo/repositories/ActivityLogRepository.java
@@ -1,13 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.ActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.ActivityLog;
-
-public interface ActivityLogRepository
-        extends JpaRepository<ActivityLog, Long> {
+public interface ActivityLogRepository extends JpaRepository<ActivityLog, Long> {
 
     List<ActivityLog> findByUser_Id(Long userId);
 
--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -2,13 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // REQUIRED BY HIDDEN TESTS
     boolean existsByEmail(String email);
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
