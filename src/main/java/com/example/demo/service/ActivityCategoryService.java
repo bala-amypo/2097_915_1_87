99e668b793876500d60418be37e82e86a02db391
bala-amypo <bala@amypo.in>
2025-12-21 06:58:01 +0000

    User commit

--- src/main/java/com/example/demo/service/ActivityCategoryService.java
+++ src/main/java/com/example/demo/service/ActivityCategoryService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ActivityCategory;
+
 import java.util.List;
 
 public interface ActivityCategoryService {
+
     ActivityCategory createCategory(ActivityCategory category);
+
     ActivityCategory getCategory(Long id);
+
     List<ActivityCategory> getAllCategories();
-    ActivityCategory updateCategory(Long id, ActivityCategory category);
-    void deleteCategory(Long id);
 }
--- src/main/java/com/example/demo/service/ActivityTypeService.java
+++ src/main/java/com/example/demo/service/ActivityTypeService.java
@@ -1,12 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ActivityType;
+
 import java.util.List;
 
 public interface ActivityTypeService {
+
     ActivityType createType(Long categoryId, ActivityType type);
+
     ActivityType getType(Long id);
+
     List<ActivityType> getTypesByCategory(Long categoryId);
-    ActivityType updateType(Long id, ActivityType type);
-    void deleteType(Long id);
 }
